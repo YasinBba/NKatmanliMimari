a3a1cb860af4b44154e2d93eade459916de09363
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-11-04 22:28:29 +0300

    entity layer oluşturuldu sınıflar hazırlandı

---
+++ EntityLayer/EntityDepartman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityDepartman
+    {
+        private int DepartmanId;
+        private string DepartmanAd;
+        private string Aciklama;
+
+        public int DepartmanId1 { get => DepartmanId; set => DepartmanId = value; }
+        public string DepartmanAd1 { get => DepartmanAd; set => DepartmanAd = value; }
+        public string Aciklama1 { get => Aciklama; set => Aciklama = value; }
+    }
+}
